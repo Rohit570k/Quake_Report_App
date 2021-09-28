e4d9029dc5f7ea32f1c29cd4dde1847a85250c2d
ROHIT KUMAR <rk15072000@gmail.com>
2021-09-28 08:52:14 +0530

    From AsyncTask to AsyncTaskLoader

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,42 @@
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EarthquakeLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public EarthquakeLoader(Context context,String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        // Don't perform the request if there are no URLs, or the first URL is null
+        if ((mUrl == null)) {
+            return null;
+        }
+
+        List<Earthquake> result = QueryUtils.fetchEarthquakeData(mUrl);
+        return result;
+    }
+}
